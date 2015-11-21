eae16fc52ce3f6f9794d46040778842d483fecc5
The-King <hunar911@gmail.com>
2015-11-21 15:27:16 -0800

    added plugindescription stuff

---
+++ src/main/java/ca/kinggoesgaming/ssessentials/sponge/PluginDescription.java
@@ -0,0 +1,14 @@
+package ca.kinggoesgaming.ssessentials.sponge;
+
+/**
+ * General plugin description.
+ */
+public class PluginDescription {
+
+    private PluginDescription() {}
+
+    public static final String ID = "ssessentials";
+    public static final String NAME = "SSEssentials";
+    public static final String VERSION = "1.0-SNAPSHOT";
+    public static final String DEPENDENCIES = "";
+}
